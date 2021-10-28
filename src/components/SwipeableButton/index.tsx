f4370e6807dc09bbafa27b55c4c94b3d286493ea
willer.rodrigo <willerrodrigo2006@hotmail.com>
2021-10-28 15:09:04 -0300

    feat(swipeable button): change flatlist by scrollview

--- src/components/SwipeableButton/index.tsx
+++ src/components/SwipeableButton/index.tsx
@@ -1,99 +1,57 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useRef, useState } from 'react';
-import {
-  Animated,
-  FlatList,
-  ListRenderItem,
-  useWindowDimensions,
-} from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Animated, useWindowDimensions } from 'react-native'
+import { Swipeable } from 'react-native-gesture-handler'
 
-import { IBook } from '../../screens/Home';
+import { IBook } from '../../screens/Home'
 
-import * as S from './styles';
+import * as S from './styles'
 
 type SwipeableButtonProps = {
-  book: IBook;
-};
+  book: IBook
+}
 
 const SwipeableButton = ({
   children,
-  book,
+  book
 }: React.PropsWithChildren<SwipeableButtonProps>) => {
-  const window = useWindowDimensions();
-  const navigation = useNavigation();
-  const listRef = useRef<FlatList>(null);
-
-  const [nextListIndex, setNextListIndex] = useState(0);
-
-  const handleScrollLeft = () => {
-    const isBiggerThanZero = nextListIndex > 0;
-
-    if(isBiggerThanZero) {
-      setNextListIndex(prev => prev - 1);
-      listRef.current?.scrollToIndex({ index: nextListIndex - 1, animated: true });
-    }
-  }
-
-  const handleScrollRight = () => {
-    const isLessThanChaptersLength = nextListIndex < book.chaptersNumber.length - 4;
-
-    if(isLessThanChaptersLength) {
-      setNextListIndex(prev => prev + 1);
-      listRef.current?.scrollToIndex({ index: nextListIndex + 1, animated: true });
-    }
-  }
+  const window = useWindowDimensions()
+  const navigation = useNavigation()
 
   const handleNavigate = (index: number) => {
     navigation.navigate('Book', {
       bookName: book.name,
-      initialScrollIndex: index,
-    });
-  };
-
-  const renderItem: ListRenderItem<number> = ({ index }) => (
-    <S.Item onPress={() => handleNavigate(index)}>
-      <S.ItemContent>{index + 1}</S.ItemContent>
-    </S.Item>
-  );
+      initialScrollIndex: index
+    })
+  }
 
   const renderRightActions = (progress: Animated.AnimatedInterpolation) => {
     const translateX = progress.interpolate({
       inputRange: [0, 1],
       outputRange: [window.width, 0],
-      extrapolate: 'clamp',
-    });
+      extrapolate: 'clamp'
+    })
 
     return (
-      <S.ListContainer
+      <S.Container
         style={{
-          transform: [{ translateX }],
+          transform: [{ translateX }]
         }}
       >
-        <S.Item onPress={handleScrollLeft}>
-          <S.ItemContent>&lt;</S.ItemContent>
-        </S.Item>
-
-        <FlatList
-          ref={listRef}
-          data={book.chaptersNumber}
-          keyExtractor={chapter => chapter.toString()}
-          renderItem={renderItem}
-          horizontal
-          scrollEventThrottle={16}
-          nestedScrollEnabled
-        />
-
-        <S.Item onPress={handleScrollRight}>
-          <S.ItemContent>&gt;</S.ItemContent>
-        </S.Item>
-      </S.ListContainer>
-    );
-  };
+        <S.List horizontal nestedScrollEnabled>
+          {book.chaptersNumber.map((_, index) => (
+            <S.Item key={index} onPress={() => handleNavigate(index)}>
+              <S.ItemContent>{index + 1}</S.ItemContent>
+            </S.Item>
+          ))}
+        </S.List>
+      </S.Container>
+    )
+  }
 
   return (
-    <Swipeable renderRightActions={renderRightActions} containerStyle={{ overflow: 'hidden'}}>{children}</Swipeable>
-  );
-};
+    <Swipeable renderRightActions={renderRightActions}>{children}</Swipeable>
+  )
+}
 
-export default SwipeableButton;
+export default SwipeableButton
--- src/components/SwipeableButton/styles.ts
+++ src/components/SwipeableButton/styles.ts
@@ -1,20 +1,25 @@
-import { Animated } from 'react-native';
-import styled from 'styled-components/native';
+import { Animated } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import styled from 'styled-components/native'
+
+export const Container = styled(Animated.View)`
+  flex: 1;
+  flex-direction: row;
+`
+
+export const List = styled(ScrollView)`
+  margin-left: 40px;
+  background-color: rgba(0, 0, 0, 0.1);
+`
 
 export const Item = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
   padding: 0px 24px;
-`;
+`
 
 export const ItemContent = styled.Text`
   font-size: 28px;
   font-family: 'Cardo_400Regular_Italic';
   color: #3d3424;
-`;
-
-export const ListContainer = styled(Animated.View)`
-  flex: 1;
-  flex-direction: row;
-  background-color: rgba(0, 0, 0, 0.1);
-`;
+`
