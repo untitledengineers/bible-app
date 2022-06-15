5a8ad4a3643c2a78f9666956998707971342566b
Willer Rodrigo <willerrodrigo2006@hotmail.com>
2022-06-15 14:53:16 -0300

    ref(utils): refactor paginate

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -1,13 +1,9 @@
-import { Chapter, PAGE_SIZE } from '../screens/Book'
-
 // paginate([1, 2, 3, 4, 5, 6], 2, 2); [3, 4]
 // paginate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4, 1); [1,2,3,4]
 
-function paginate(
-  array: Chapter[],
-  page_number: number,
-  page_size: number = PAGE_SIZE
-) {
+const PAGE_SIZE = 10
+
+function paginate(array: string[], page_number: number, page_size = PAGE_SIZE) {
   // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
   return array.slice((page_number - 1) * page_size, page_number * page_size)
 }
