5fabb681bad425784e47eeeac650bcd7be6800b0
Willer Rodrigo <willerrodrigo2006@hotmail.com>
2022-06-15 13:15:22 -0300

    chore(app): ignore warnings

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import React from 'react'
 
+import './src/utils/ignoreWarnings'
+
 import App from './src'
 
 const Main = () => {
---
+++ src/utils/ignoreWarnings.ts
@@ -0,0 +1,6 @@
+import { LogBox } from 'react-native'
+
+LogBox.ignoreLogs([
+  'ViewPropTypes will be removed',
+  'EventEmitter.removeListener'
+])
